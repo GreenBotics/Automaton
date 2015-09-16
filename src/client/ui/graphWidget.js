df1aa6e0b2b17e58717b656d7c9f4801864f068a
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-09-16 20:21:00 +0200

    - cleaned up graphWidget experimental implementation :  * removed any test specific code from widget  * cleaned up internals  * added good handling of defaults / params

--- src/client/ui/graphWidget.js
+++ src/client/ui/graphWidget.js
@@ -4,17 +4,31 @@ require("metrics-graphics/dist/metricsgraphics.css")
 //var d3 = require('metrics-graphics/node_modules/d3')
 
 
-function GraphWidget(data) {
+function GraphWidget(data, settings) {
     this.type = 'Widget'
-    
-    function formatEntry(entry){
-      return entry.map( (e,index)=>({time: index+'',temperature:Math.abs(e*30)}) ) 
-    }
-    //data = data.map((e,index)=>({time: index+'',temperature:Math.abs(e*15) } ) )
-    data = data.map(formatEntry)
 
-    this.data = data //MG.convert.date(data, 'time','%S')
-    //console.log(JSON.stringify(this.data))
+    const defaults = {
+        title: undefined,
+        description:undefined,
+        width:650,
+        height:150,
+        x_accessor: undefined,
+        y_accessor: undefined,
+        max_x:25,
+
+        //markers: [{'year': 1964, 'label': '"The Creeping Terror" released'}],
+        animate_on_load:false,
+        baselines: undefined,
+        legend:undefined,
+        interpolate: 'linear',
+        missing_is_zero: true,
+        transition_on_update:false,//disable for very high frequency data updates (no time for transition)
+        show_tooltips:false//no jquery please
+    }
+
+    this.data = data
+
+    this.settings  = Object.assign(defaults,settings,{data})
 }
 
 GraphWidget.prototype.init = function () {
@@ -23,30 +37,9 @@ GraphWidget.prototype.init = function () {
 
 GraphWidget.prototype.update = function (prev, elem) {
   //this.graph = this.graph || prev.graph
-  let adi_baselines = [{value:12, label:'critical temperature'}];
+  let settings = Object.assign(this.settings,{target:elem})
 
-  this.graph = MG.data_graphic({
-        title: "Temperatures",
-        description: "Temperature curves for env#0",
-        data: this.data,
-        width: 650,
-        height: 150,
-        target: elem,
-        x_accessor: 'time',
-        y_accessor: 'temperature',
-        max_x:25,
-
-
-        //markers: [{'year': 1964, 'label': '"The Creeping Terror" released'}],
-        animate_on_load:false,
-        baselines: adi_baselines,
-        legend:["t°0", "t°1"],
-        interpolate: 'linear',
-        missing_is_zero: true,
-        //transition_on_update:false,//disable for very high frequency data updates (no time for transition)
-        show_tooltips:false//no jquery please
-
-    })
+  this.graph = MG.data_graphic(settings)
 }
 
 export {GraphWidget}
