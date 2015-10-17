63e0466ba5ebd33e60020e5061cb734e63353666
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-10-17 16:28:30 +0200

    refactor(model): cleaned up & extracted code from main model - also various cleanups

--- src/client/index.js
+++ src/client/index.js
@@ -2,18 +2,7 @@
 import Cycle from '@cycle/core'
 import {Rx} from '@cycle/core'
 import {makeDOMDriver, hJSX} from '@cycle/dom'
-
 import SocketIO from 'cycle-socket.io'
-
-//import {renderRelays, renderCoolers, renderSensors, renderHistory, renderSensorData} from './ui/uiElements'
-//import {coolers, labeledInputSlider, wrapper} from './ui/nested'
-//import {coolers, labeledInputSlider, mainView} from './ui/custom'
-
-//import {GlWidget} from './ui/glWidget'
-//import {GraphWidget} from './ui/graphWidget'
-
-//import {model, intent} from './model'
-//import {history, historyIntent} from './history'
 
 import main from './components/main'
 
--- src/client/model.js
+++ src/client/model.js
@@ -1,82 +1,9 @@
 import Immutable from 'seamless-immutable'
 import {Rx} from '@cycle/core'
-//import Immutable from 'immutable'
 import "babel-core/polyfill"
 
 import {makeModel, makeModifications} from './model/modelHelper'
 import {mergeData} from './utils'
-
-function getId(e){
-  let id = parseInt( e.target.id.split("_").pop() )
-  return {id}
-}
-
-function idAndChecked(e){
-  let id = parseInt( e.target.id.split("_").pop() )
-  return {id,toggled:e.target.checked}
-}
-
-function idAndValue(e){
-  let id = parseInt( e.target.id.split("_").pop() )
-  //let value = parseFloat(e.target.value)// for basic use case
-  let value = parseFloat(e.detail)//for custom element etc
-  return {id,value}
-}
-
-export function intent(DOM, other){
-  let toggleRelay$ =  DOM.select('.relayToggler').events('click')
-    .map(idAndChecked)
-
-  let removeRelay$ = DOM.select('.removeRelay').events('click')
-    .map(getId)
-
-  let removeAllRelays$ = DOM.select('#removeAllRelays').events('click')
-    .map(true)
-
-  let setCoolerPower$ = DOM.select('.coolerSlider').events('input')//input vs change events
-    //.merge( DOM.select('.coolerSlider_number'.events('change') )
-    .merge( DOM.select('.labeled-input-slider-cooler').events('change') )
-    //DOM.select('.coolerSlider_number','change')
-    .debounce(30)
-    .map(idAndValue)
-    .do(e=>console.log("value",e))*/
-
-  let setCoolerPower$ = other.setCoolerPower$//.debounce(30)
-
-  DOM.select('.labeled-input-slider-cooler').events('newValue')
-    .subscribe(e=>console.log("saw cooler slider change",e))
-
-  let emergencyShutdown$ = DOM.select('#shutdown').events('click')
-    .map(false)
-
-  let toggleSensor$ = DOM.select('.sensorToggler').events('click')
-    .map(idAndChecked)
-
-  
-  //////////
-  let undo$ = DOM.select('#undo').events('click')
-    .map(true)
-
-  let redo$ = DOM.select('#redo').events('click')
-    .map(false)
-
-
-  return {
-    toggleRelay$
-    ,removeRelay$
-    ,removeAllRelays$
-
-    ,emergencyShutdown$
-
-    ,setCoolerPower$
-    ,toggleSensor$
-
-    , undo$
-    , redo$}
-}
-
-
-//////////////////////////////////////
 
 //these all are actual "api functions"  
 function toggleRelay(state, input){
@@ -88,7 +15,7 @@ function toggleRelay(state, input){
       return relay
     })
 
-  state = mergeData( state, {active:true, relays})//toggleRelays(state,toggleInfo) )
+  state = mergeData( state, {active:true, relays})
   return state
 }
 
@@ -141,12 +68,12 @@ function toggleSensor(state, input){
       return sensor
     })
 
-  state = mergeData( state, {active:true, sensors})//toggleRelays(state,toggleInfo) )
+  state = mergeData( state, {active:true, sensors})
   return state
 }
 
 
-export function model(actions){
+export default function model(actions){
 
     const defaults = { 
       state:{
@@ -199,15 +126,14 @@ export function model(actions){
 
     //for testing
     let sensor1Data$ = Rx.Observable
-      .interval(100 /* ms */)
+      .interval(100 )//ms
       .timeInterval()
       .map(e=> Math.random())
 
     let sensor2Data$ = Rx.Observable
-      .interval(500 /* ms */)
+      .interval(500 )//ms
       .timeInterval()
       .map(e=> Math.random())
 
     return makeModel(updateFns, actions, defaults)
-   
 }
