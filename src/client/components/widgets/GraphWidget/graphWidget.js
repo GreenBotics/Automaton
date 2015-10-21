0836564e1945b395ce2da08c221b07985e64baf4
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-10-21 08:38:12 +0200

    feature(graphWidget): modified GraphWidget to enable "manually" setting the data after instance creation, to avoid re-creating it all the time

--- src/client/components/widgets/GraphWidget/graphWidget.js
+++ src/client/components/widgets/GraphWidget/graphWidget.js
@@ -14,9 +14,11 @@ function GraphWidget(data, settings) {
         description:undefined,
         //width:650,
         height:150,
-        x_accessor: undefined,
-        y_accessor: undefined,
-        max_x:25,
+        x_accessor: 'time',
+        y_accessor: 'value',
+        max_x:undefined,
+
+        data: [],
 
         //markers: [{'year': 1964, 'label': '"The Creeping Terror" released'}],
         animate_on_load:false,
@@ -25,7 +27,9 @@ function GraphWidget(data, settings) {
         interpolate: 'linear',
         missing_is_zero: true,
         transition_on_update:false,//disable for very high frequency data updates (no time for transition)
-        show_tooltips:false//no jquery please
+        show_tooltips:false,//no jquery please
+
+        //linked_format: "%Y-%m-%d-%H-%M-%S",
     }
 
     this.data = data
@@ -36,9 +40,22 @@ function GraphWidget(data, settings) {
 }
 
 GraphWidget.prototype.init = function () {
+  console.log("INIT GraphWidget")
   let elem = document.createElement('div')
   elem.className = "graph"
+  this.elem = elem 
+  this.settings = Object.assign(this.settings,{target:elem})
+  this.draw()
   return elem
+}
+
+GraphWidget.prototype.updateData = function (data) {
+  console.log("updateData GraphWidget")
+  if(data){
+    //data = MG.convert.date(data, 'time', '%Y-%m-%dT%H:%M:%S');
+    this.settings = Object.assign(this.settings,{data})
+  }
+  this.draw()
 }
 
 GraphWidget.prototype.update = function (prev, elem) {
@@ -48,6 +65,10 @@ GraphWidget.prototype.update = function (prev, elem) {
   let settings = Object.assign(this.settings,{target:elem})
 
   this.graph = MG.data_graphic(settings)
+}
+
+GraphWidget.prototype.draw = function(){
+  this.graph = MG.data_graphic(this.settings)
 }
 
 export {GraphWidget}
