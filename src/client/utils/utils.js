4c6899a5c5e5ac4607cd653b892d055f0a8bb138
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-02-28 14:09:36 +0100

    feature(uuid): added uuid generator to client's utils

--- src/client/utils/utils.js
+++ src/client/utils/utils.js
@@ -29,3 +29,35 @@ export function findIdenticals(equals, listA, listB){
   })
   return result
 }
+
+//TODO: taken from three.js ,do correct attribution
+export function generateUUID() {
+  // http://www.broofa.com/Tools/Math.uuid.htm
+  let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split( '' );
+  let uuid = new Array( 36 );
+  let rnd = 0, r;
+
+  return function () {
+
+    for ( let i = 0; i < 36; i ++ ) {
+
+      if ( i == 8 || i == 13 || i == 18 || i == 23 ) {
+
+        uuid[ i ] = '-';
+
+      } else if ( i == 14 ) {
+
+        uuid[ i ] = '4';
+
+      } else {
+
+        if ( rnd <= 0x02 ) rnd = 0x2000000 + ( Math.random() * 0x1000000 ) | 0;
+        r = rnd & 0xf;
+        rnd = rnd >> 4;
+        uuid[ i ] = chars[ ( i == 19 ) ? ( r & 0x3 ) | 0x8 : r ];
+
+      }
+    }
+    return uuid.join( '' )
+  }()
+}
