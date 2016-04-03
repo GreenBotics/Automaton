7d896aa55267ccf33b67244fd5071ce137ebb0d4
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-04-03 13:23:43 +0200

    chore(): added "events" driver boilerplate

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@
 import Cycle from '@cycle/core'
 import Rx from 'rx'
 import SocketIO from 'cycle-socket.io'
+import eventDriver from './drivers/eventDriver'
 
 //import {makeDOMDriver} from '@cycle/dom'
 import {modules, makeDOMDriver} from 'cycle-snabbdom'
@@ -17,10 +18,11 @@ import main from './components/main'
 
 //////////setup drivers
 let socketIODriver = SocketIO.createSocketIODriver(window.location.origin)
-let domDriver      = makeDOMDriver('#app',[StyleModule, PropsModule, AttrsModule, ClassModule, HeroModule, EventsModule])//makeDOMDriver('#app') // 
+let domDriver      = makeDOMDriver('#app',[StyleModule, PropsModule, AttrsModule, ClassModule, HeroModule, EventsModule])//makeDOMDriver('#app') //
 const drivers = {
    DOM: domDriver
   ,socketIO: socketIODriver
+  ,events:eventDriver
 }
 
 Cycle.run(main, drivers)
